629a97e7b1c47f89e9f81f6d2385fc23d4fe9e5f
Frederick Valone <valone.carlf@epa.gov>
2024-11-11 12:10:30 -0500

    update dependencies (#1)
    * Update README.md
    
    * remove client only
    
    * add search icon
    
    * add back to top icon
    
    * update isearch input box style to override vuetify style
    
    * upgrade dependencies
    
    * remove references to unused sprite that was causing cross-site errors on localhost https://www.epa.gov/themes/images/sprite.artifact.svg
    
    ---------
    
    Co-authored-by: Carl Valone <cvalone@d2626ucvalone3.aa.ad.epa.gov>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
 
   // To re-enable _all_ Nuxt v3 behavior, set the following options:
   // srcDir: '.',
